7ddffc41212dbcf5e589c5a52cb29c03645e5642
Bünyamin Horoz <83961562+bhoroz@users.noreply.github.com>
2022-12-08 13:54:55 +0300

    Add files via upload

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <pre>
+        Created by <a href="https://github.com/bhoroz">Bünyamin Horoz</a>
+      </pre>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/header/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import cities from "../../helpers/cities_of_turkey.json";
+import { useSelect } from "../../contexts/SelectContext";
+
+function Header() {
+  const { setSelect } = useSelect();
+
+  const handleChange = (event) => setSelect(event.target.value);
+
+  return (
+    <div>
+        <div className="headerTop">
+          The Weather APP
+        </div>
+        <div className="Header">
+          <label htmlFor="select">Cities:</label>
+
+          <select id="select" onChange={handleChange}>
+            {cities.map((city) => (
+              <option key={city.id} value={city.name}>
+                {city.name}
+              </option>
+            ))}
+          </select>
+        </div>
+    </div>
+  );
+}
+
+export default Header;
